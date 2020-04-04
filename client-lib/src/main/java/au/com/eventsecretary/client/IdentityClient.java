64da041c7304b75b61159ff629098f638315e5d2
Warwick Slade <wslade@eventsecretary.com.au>
2020-04-05 09:39:10 +1000

    Added terms and conditions

--- client-lib/src/main/java/au/com/eventsecretary/client/IdentityClient.java
+++ client-lib/src/main/java/au/com/eventsecretary/client/IdentityClient.java
@@ -1,15 +1,19 @@
 package au.com.eventsecretary.client;
 
 import au.com.eventsecretary.ResourceExistsException;
+import au.com.eventsecretary.ResourceNotFoundException;
 import au.com.eventsecretary.UnexpectedSystemException;
 import au.com.eventsecretary.user.identity.Identity;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author sladew
@@ -38,6 +42,39 @@ public class IdentityClient extends AbstractClient {
         catch (RestClientException e) {
             logger.error("get:could not connect to identity service" + e.getMessage());
             return null;
+        }
+    }
+
+    public Identity findByEmailAddress(String emailAddress) {
+        try {
+            logger.info("find:" + emailAddress);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.put(HttpHeaders.AUTHORIZATION, Collections.singletonList("820013bb-5655-42c4-8784-af94a82e668b"));
+            HttpEntity<Void> httpEntity = new HttpEntity(headers);
+
+            Map<String, String> params = new HashMap<>();
+            params.put("emailAddress", emailAddress);
+            ResponseEntity<List<Identity>> exchange = restTemplate.exchange(baseUrl + URI + "?email={emailAddress}", HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<Identity>>() {}, params);
+            switch (exchange.getStatusCode()) {
+                case OK:
+                    List<Identity> identityList = exchange.getBody();
+                    if (identityList == null || identityList.isEmpty()) {
+                        throw new ResourceNotFoundException(emailAddress);
+                    }
+                    if (identityList.size() > 1) {
+                        throw new UnexpectedSystemException("Multiple identity for " + emailAddress);
+                    }
+                    return identityList.get(0);
+                case NOT_FOUND:
+                    throw new ResourceNotFoundException(emailAddress);
+                default:
+                    throw new UnexpectedSystemException("Invalid response code:" + exchange.getStatusCode());
+            }
+        }
+        catch (RestClientException e) {
+            logger.error("get:could not connect to identity service" + e.getMessage());
+            throw new UnexpectedSystemException(e);
         }
     }
 
