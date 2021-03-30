55f72218c1bb0dfe1079491d2c62aafe9b6a63a7
Warwick Slade <wslade@eventsecretary.com.au>
2021-03-30 16:34:58 +1100

    (story) Changes for shop

---
+++ export-lib/src/main/java/au/com/eventsecretary/export/ModelUtils.java
@@ -0,0 +1,51 @@
+package au.com.eventsecretary.export;
+
+import au.com.auspost.simm.model.ComplexType;
+import au.com.auspost.simm.model.Intrinsic;
+import au.com.eventsecretary.UnexpectedSystemException;
+import au.com.eventsecretary.simm.ModelExtensions;
+
+import static au.com.eventsecretary.export.ValueFormatter.enumFormatterClassifier;
+import static au.com.eventsecretary.simm.ExtensionUtils.alias;
+import static au.com.eventsecretary.simm.ExtensionUtils.findComplexTypeById;
+
+/**
+ * TODO
+ *
+ * @author Warwick Slade
+ */
+public interface ModelUtils {
+    static SheetBuilderFunction complexTypeBuilder(ModelExtensions modelExtensions, ComplexType complexType) {
+        return sheetBuilder -> {
+            if (complexType == null) {
+                return;
+            }
+
+            complexType.getAttributes().forEach(attribute -> {
+                ColumnBuilder builder = sheetBuilder.column().label(alias(attribute));
+                switch (attribute.getType()) {
+                    case ENUM:
+                        ComplexType enumComplexType = findComplexTypeById(modelExtensions.getComplexTypeExtensions(), attribute.getClassifier());
+                        builder.stringFormat(enumFormatterClassifier(enumComplexType));
+                        break;
+                    case INTRINSIC:
+                        if (attribute.getClassifier().equals(Intrinsic.BOOLEAN.name())) {
+                            builder.booleanFormat();
+                        } else if (attribute.getClassifier().equals(Intrinsic.STRING.name())) {
+                            builder.stringFormat();
+                        } else if (attribute.getClassifier().equals(Intrinsic.INTEGER.name())) {
+                            builder.integerFormat();
+                        } else if (attribute.getClassifier().equals(Intrinsic.DECIMAL.name())) {
+                            builder.numericFormat();
+                        } else {
+                            throw new UnexpectedSystemException("IntrinsicNotSupported:" + attribute.getClassifier());
+                        }
+                        break;
+                    default:
+                        throw new UnexpectedSystemException("TypeNotSupported:" + attribute.getType());
+                }
+                builder.end();
+            });
+        };
+    }
+}
---
+++ simm-lib/src/main/java/au/com/eventsecretary/simm/ModelUtils.java
@@ -0,0 +1,32 @@
+package au.com.eventsecretary.simm;
+
+import au.com.auspost.simm.model.Attribute;
+import au.com.auspost.simm.model.ComplexType;
+import au.com.auspost.simm.model.Type;
+
+import java.util.Map;
+
+/**
+ * TODO
+ *
+ * @author Warwick Slade
+ */
+public interface ModelUtils {
+
+    static String extensionValue(ModelExtensions modelExtensions, Attribute attribute, Map<String, String> extensions) {
+        if (extensions == null) {
+            return "";
+        }
+        String value = extensions.get(attribute.getId());
+        if (attribute.getType() == Type.ENUM && value != null) {
+            ComplexType enumComplexType = modelExtensions.getComplexTypeExtensions().stream().filter(ct -> ct.getId().equals(attribute.getClassifier())).findFirst().get();
+            for (Attribute enumAttribute : enumComplexType.getAttributes()) {
+                if (enumAttribute.getId().equals(value)) {
+                    value = ExtensionUtils.alias(enumAttribute);
+                    break;
+                }
+            }
+        }
+        return value;
+    }
+}
