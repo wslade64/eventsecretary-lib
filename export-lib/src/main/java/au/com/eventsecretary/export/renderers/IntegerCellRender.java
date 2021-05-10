f8e11e4503d855b61c273393ffe96ddea252766a
Warwick Slade <wslade@eventsecretary.com.au>
2021-05-10 20:44:46 +1000

    (defect) Fixed issue with string ints

--- export-lib/src/main/java/au/com/eventsecretary/export/renderers/IntegerCellRender.java
+++ export-lib/src/main/java/au/com/eventsecretary/export/renderers/IntegerCellRender.java
@@ -7,13 +7,24 @@ import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 
-public class IntegerCellRender implements CellRenderer<Integer> {
+public class IntegerCellRender implements CellRenderer<Object> {
 
     @Override
-    public void render(Cell cell, Integer value, WorkbookBuilder workbookBuilder) {
+    public void render(Cell cell, Object value, WorkbookBuilder workbookBuilder) {
+        if (value != null) {
+            if (value instanceof String) {
+                try {
+                    value = Integer.parseInt((String)value);
+                } catch (NumberFormatException e) {
+                    value = null;
+                }
+            } else if (!(value instanceof Integer)) {
+                value = null;
+            }
+        }
         cell.setCellType(CellType.NUMERIC);
         if (value != null) {
-            cell.setCellValue(value);
+            cell.setCellValue((Integer)value);
         }
 
         CellStyle cellStyle = cell.getCellStyle();
