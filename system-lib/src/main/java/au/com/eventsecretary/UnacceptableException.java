f7b38572c815983ec7b93a178aaacdd5c7c0cb2b
Warwick Slade <wslade@eventsecretary.com.au>
2021-06-25 17:08:39 +1000

    (story) Reworking of import service.

---
+++ system-lib/src/main/java/au/com/eventsecretary/UnacceptableException.java
@@ -0,0 +1,10 @@
+package au.com.eventsecretary;
+
+/**
+ * @author sladew
+ */
+public class UnacceptableException extends RuntimeException {
+    public UnacceptableException(String message) {
+        super(message);
+    }
+}
