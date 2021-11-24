611e9b3347d957452fb0133aa92e3d06c2f442a4
Warwick Slade <wslade@eventsecretary.com.au>
2021-11-24 13:21:36 +1100

    (change) Validation for HRCAV shows. Include ResultStatus in EventPhaseStatus

--- simm-lib/src/main/java/au/com/eventsecretary/simm/ModelUtils.java
+++ simm-lib/src/main/java/au/com/eventsecretary/simm/ModelUtils.java
@@ -2,8 +2,12 @@ package au.com.eventsecretary.simm;
 
 import au.com.auspost.simm.model.Attribute;
 import au.com.auspost.simm.model.ComplexType;
+import au.com.auspost.simm.model.Documentation;
+import au.com.auspost.simm.model.Extension;
 import au.com.auspost.simm.model.Type;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,5 +35,24 @@ public interface ModelUtils {
             }
         }
         return value;
+    }
+
+    static Documentation findDocumentation(Attribute attribute) {
+        for (Extension extension : attribute.getExtension()) {
+            if (extension instanceof Documentation) {
+                return (Documentation) extension;
+            }
+        }
+        return null;
+    }
+
+    static Attribute findAttributeByAlias(List<Attribute> attributes, String alias) {
+        for (Attribute attribute : attributes) {
+            Documentation documentation = findDocumentation(attribute);
+            if (documentation != null && StringUtils.equals(documentation.getAlias(), alias)) {
+                return attribute;
+            }
+        }
+        return null;
     }
 }
