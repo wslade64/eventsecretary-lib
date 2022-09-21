4df3190d68e49156ecfa95ee0b41ae9c2c67bec4
Warwick Slade <wslade@eventsecretary.com.au>
2022-09-21 18:22:21 +1000

    log to security.

--- system-lib/src/main/java/au/com/eventsecretary/Audit.java
+++ system-lib/src/main/java/au/com/eventsecretary/Audit.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
  */
 public class Audit {
     private static final Logger logger = LoggerFactory.getLogger("audit");
+    private static final Logger security = LoggerFactory.getLogger("security");
 
     private static final String[] param = {
              "{}",
@@ -25,5 +26,8 @@ public class Audit {
 
     public static void audit(Object... values) {
         logger.info(param[values.length-1], values);
+    }
+    public static void security(Object... values) {
+        security.info(param[values.length-1], values);
     }
 }
