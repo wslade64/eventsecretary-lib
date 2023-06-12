a2dc283d09ad0a41051788afa115c3ef21c46437
Warwick Slade <wslade64@gmail.com>
2023-06-13 07:34:51 +1000

    Moved result model classes Added FEI XML classes.

--- export-lib/src/main/java/au/com/eventsecretary/export/XmlBuilder.java
+++ export-lib/src/main/java/au/com/eventsecretary/export/XmlBuilder.java
@@ -1,6 +1,7 @@
 package au.com.eventsecretary.export;
 
 import au.com.eventsecretary.UnexpectedSystemException;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
@@ -19,6 +20,8 @@ public class XmlBuilder implements FileBuilder {
     public String build(File file) {
         XmlMapper xmlMapper = new XmlMapper();
         xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        xmlMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
         try (FileOutputStream fos = new FileOutputStream(file)){
             xmlMapper.writeValue(fos, object);
             return "xml";
