3cf1fe91cf074cea805f3aae9af587795e0b6567
Warwick Slade <wslade64@gmail.com>
2023-12-08 17:57:35 +1100

    Add checks to ensure that orders balance when finalising.

--- client-lib/src/main/java/au/com/eventsecretary/client/AccountClient.java
+++ client-lib/src/main/java/au/com/eventsecretary/client/AccountClient.java
@@ -5,10 +5,13 @@ import au.com.eventsecretary.UnexpectedSystemException;
 import au.com.eventsecretary.accounting.account.Account;
 import au.com.eventsecretary.people.presentation.PersonIdentity;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClientException;
+
+import java.util.List;
 
 /**
  * @author sladew
@@ -121,6 +124,28 @@ public class AccountClient extends AbstractClient {
 
             ResponseEntity<Account> exchange = restTemplate.exchange(baseUrl + URI + "/" + accountCode
                 , HttpMethod.GET, httpEntity, Account.class);
+            switch (exchange.getStatusCode()) {
+                case OK:
+                    return exchange.getBody();
+                case NOT_FOUND:
+                    throw new ResourceNotFoundException("account");
+                default:
+                    throw new UnexpectedSystemException("Invalid response code:" + exchange.getStatusCode());
+            }
+        }
+        catch (RestClientException e) {
+            logger.error("could not connect to service" + e.getMessage());
+            throw new UnexpectedSystemException(e);
+        }
+    }
+
+    public List<String> verifyAccount(String accountCode) {
+
+        try {
+            HttpEntity<Void> httpEntity = createSystemEntity();
+
+            ResponseEntity<List<String>> exchange = restTemplate.exchange(baseUrl + "/payment/v1/admin?accountCode=" + accountCode
+                    , HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<String>>(){});
             switch (exchange.getStatusCode()) {
                 case OK:
                     return exchange.getBody();
